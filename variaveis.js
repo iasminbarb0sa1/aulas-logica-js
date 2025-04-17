61b4f35d583a4e51e8f42e1290b06515c7e5e785
IASMIN BARBOSA SOUTO <iasmin_souto@estudante.sesisenai.org.br>
2025-04-16 22:18:52 -0300

    declarção de variáveis

---
+++ variaveis.js
@@ -0,0 +1,19 @@
+//declaração de veriável com var
+
+//var nome = "Iasmin"
+
+//var nome = "João"
+
+//declaração de variável com let
+
+//let nome  = "Iasmin"
+
+//nome = "João"
+
+//variáveç declarada com const
+
+const nome = "Iasmin"
+
+nome = "João"
+
+console.log(nome)
